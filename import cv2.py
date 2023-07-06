d54580da1b092c978501d081e557b5055e38fa6c
biditsadhukhan <sadhukhanbidit@gmail.com>
2023-07-06 16:27:47 +0530

    initial commit

---
+++ import cv2.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ab3faa832c9bddccfdbd6ae570185d986321c24718fc860c44c26899e63ebe07
+size 34
---
+++ motiondetection.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:96c23f0e9bbc0c2b66f082df80206be532e3e7fbcd129990dc36df0c8efa50b4
+size 1222
